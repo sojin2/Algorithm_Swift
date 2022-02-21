45951e6283662f7578a1044a98e31bd4676a7381
sojin2 <jin980729@gmail.com>
2022-02-22 01:17:54 +0900

    20220221

--- 수박수박수박수?.playground/Contents.swift
+++ 수박수박수박수?.playground/Contents.swift
@@ -1,5 +1,25 @@
 import UIKit
 
 func solution(_ n:Int) -> String {
+    
+    // 내가 푼 것
+    var result = ""
+
+      for i in 1...n {
+          if i % 2 == 1 {
+              result += "수"
+          } else {
+              result += "박"
+          }
+      }
+
+      return result
+    
+    // 좋은 풀이
+    
+    return "\(String(repeating: "수박", count: n / 2))\(n % 2 == 0 ? "" : "수")"
+    
+    return (0..<n).map{($0%2==0 ? "수":"박")}.reduce("", +)
+    
     return String(String(repeating: "수박", count: (n+1) / 2).prefix(n))
 }
