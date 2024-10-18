11b1e90f9c8a21583aa4fdcc282218d832feb8f2
SojinKim <60097304+sojin2@users.noreply.github.com>
2024-10-18 16:22:18 +0900

    Title: 100점 도달하기, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 241018/100점 도달하기/reach-100-point.swift
+++ 241018/100점 도달하기/reach-100-point.swift
@@ -1,20 +1,20 @@
-if let input = readLine(), var score = Int(input) {
-    var result = ""
-    for n in score...100 {
-        switch score {
-            case 90...100:
-                result.append("A")
-            case 80..<90:
-                result.append("B")
-            case 70..<80:
-                result.append("C")
-            case 60..<70:
-                result.append("D")
-            default:
-                result.append("F")
-        }
-        result.append(" ")
-        score += 1
+var input = Int(readLine()!)!
+
+var result = ""
+for n in input...100 {
+    switch input {
+        case 90...100:
+            result.append("A")
+        case 80..<90:
+            result.append("B")
+        case 70..<80:
+            result.append("C")
+        case 60..<70:
+            result.append("D")
+        default:
+            result.append("F")
     }
-    print(result)
+    result.append(" ")
+    input += 1
 }
+print(result)
